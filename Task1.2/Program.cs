5db1e7ea7051550b875c9f3e4fcf299f9d43ed9c
New <New@LAPTOP-LAEBPE6Q>
2023-11-30 02:31:01 +0200

    Добавьте файлы проекта.

---
+++ Task1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+    class Program
+{
+static void Main()
+    {
+ Console.Write("12,345"); //Надається довільне значення. Приклад:12,34       
+double number = Convert.ToDouble(Console.ReadLine());
+        int decimalPart = (int)((number - Math.Floor(number)) * 100);
+    int digit1 = decimalPart / 10;
+    int digit2 = decimalPart % 10;
+    int sum = digit1 + digit2; //Задаємо умови для розділення числа на цифри та виведення суми перших дву цифр числа після коми
+    Console.WriteLine($"{digit1} + {digit2} = {sum}");
+        }
+}
